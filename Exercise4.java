ac34435abbe136449581f7613a04f7c93a71ed03
KISSLASS <G00440483@atu.ie>
2025-09-30 16:43:56 +0000

    Lab1

---
+++ Exercise4.java
@@ -0,0 +1,6 @@
+public class Exercise4{
+    public static void main(String[] args){
+     System.out.println("The capital of Ireland is:\tDublin");
+    System.out.println("The patron saint of Ireland is:\tSaint Patrick");
+     }
+}
---
+++ Exercise5.java
@@ -0,0 +1,8 @@
+public class Exercise5{
+    public static void main(String[] args){
+     
+     System.out.println("A\tB");
+     System.out.println("=========");
+     System.out.println("1\t2");
+     }
+}
